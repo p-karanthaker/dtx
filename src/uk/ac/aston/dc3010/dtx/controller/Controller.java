429be88cd4357d4b9dca9843c09a8a92c9ec1653
Karan Thaker <karan.thaker@hotmail.co.uk>
2018-07-30 12:00:33 +0100

    Frontend Development

---
+++ src/uk/ac/aston/dc3010/dtx/controller/Controller.java
@@ -0,0 +1,58 @@
+package uk.ac.aston.dc3010.dtx.controller;
+
+import uk.ac.aston.dc3010.dtx.model.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Controller extends HttpServlet {
+
+  private Users users;
+  private HttpSession session;
+
+  public void init() {
+    users = new Users();
+  }
+
+  public void destroy() {
+
+  }
+
+  protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    final String action = req.getPathInfo();
+    final String contextPath = req.getContextPath();
+    final String path = contextPath + req.getServletPath();
+    session = req.getSession(true);
+
+    switch(action) {
+      case "/login":
+        final String username = req.getParameter("username");
+        final String password = req.getParameter("password");
+        Users users = new Users();
+        if (users.authenticate(username, password)) {
+          session.setAttribute("user", username);
+
+          resp.sendRedirect(contextPath + "/index.jspx");
+        } else {
+          resp.sendRedirect(contextPath + "/login.jsp");
+        }
+        break;
+      case "/logout":
+        session.invalidate();
+        resp.sendRedirect(contextPath + "/login.jsp");
+      default:
+        break;
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    processRequest(req, resp);
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    processRequest(req, resp);
+  }
+}
---
+++ src/uk/ac/aston/dc3010/dtx/model/Users.java
@@ -0,0 +1,53 @@
+package uk.ac.aston.dc3010.dtx.model;
+
+import javax.naming.*;
+import javax.sql.*;
+import java.sql.*;
+
+public class Users {
+
+  private DataSource dataSource = null;
+
+  public Users() {
+
+    try {
+      Context context = new InitialContext();
+      Context environment = (Context) context.lookup("java:comp/env");
+
+      dataSource = (DataSource) environment.lookup("jdbc/DTXDatabase");
+    } catch (Exception e) {
+      System.out.println("Exception: " + e.getMessage());
+    }
+
+  }
+
+  public boolean authenticate(String username, String password) {
+    try (Connection connection = dataSource.getConnection()) {
+      if (connection != null) {
+        final String query = "SELECT username, password FROM employee WHERE username = ? AND password = ?";
+
+        final PreparedStatement ps = connection.prepareStatement(query);
+        ps.setString(1, username);
+        ps.setString(2, password);
+
+        final ResultSet rs = ps.executeQuery();
+        String user = null;
+        String pword = null;
+        while (rs.next()) {
+          user = rs.getString("username");
+          pword = rs.getString("password");
+        }
+
+        if ((user != null && pword != null) && (user.equals(username) && pword.equals(password))) {
+          return true;
+        }
+
+      }
+    } catch (SQLException e) {
+      System.out.println("Exception: " + e.getMessage());
+    }
+
+    return false;
+  }
+
+}
