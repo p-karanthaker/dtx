dbc526d8820a12a2d33d1522cf8e7199609f5746
Karan Thaker <karan.thaker@hotmail.co.uk>
2018-08-03 00:04:49 +0100

    New items
    Calendar grid improved.
    Added info panel to right. More work to do on it.

---
+++ web/resources/js/dtx.js
@@ -0,0 +1,12 @@
+function toggleNav() {
+  var nav = document.getElementById("mySidenav");
+  if (parseInt(nav.style.width) > 0) {
+    nav.style.width = "0";
+  } else {
+    nav.style.width = "250px";
+  }
+}
+
+document.onclick = function () {
+  toggleNav();
+}
