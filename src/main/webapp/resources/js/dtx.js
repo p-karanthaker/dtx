0e0491437fccf1380ecfa8e361dba579bfcef78f
Karan Thaker <karan.thaker@hotmail.co.uk>
2018-08-03 21:21:48 +0100

    Moved datepicker script to dtx.js

--- src/main/webapp/resources/js/dtx.js
+++ src/main/webapp/resources/js/dtx.js
@@ -10,3 +10,20 @@ function toggleNav() {
 document.onclick = function () {
   //toggleNav();
 };
+
+$(document).ready(function () {
+  var dp = $('#date').datepicker({
+    autoClose: true,
+    language: "en",
+    minView: "months",
+    view: "years",
+    dateFormat: "MM yyyy"
+  }).data('datepicker');
+  dp.selectDate(new Date());
+
+  $('#date').text(new Date());
+
+  $('#datepicker').click(function () {
+    dp.show();
+  });
+});
